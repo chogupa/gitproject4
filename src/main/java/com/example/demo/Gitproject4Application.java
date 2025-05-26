db33c766f65ee6681f5cb3e08a189900e5ef2622
chogupa <j321033@g.kogakuin.jp>
2025-05-26 16:43:39 +0900

    新規作成

---
+++ src/main/java/com/example/demo/Gitproject4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitproject4Application.class, args);
+	}
+
+}
