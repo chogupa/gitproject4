1f201cb69b6d32592f725fecfe16b96345f53baf
kw2s1tr0 <k.s.sisho1122@gmail.com>
2025-05-27 15:36:48 +0900

    minesweeperの生成クラスver2

--- src/main/java/com/example/demo/form/Minesweeper.java
+++ src/main/java/com/example/demo/form/Minesweeper.java
@@ -5,19 +5,21 @@ import java.util.Collections;
 
 public class Minesweeper {
 
+	private int masu;
 	private int masume;
 	private int mine;
 
-	public void Minesweeper(int masume) {
-		this.masume = masume;
+	public Minesweeper(int masu) {
+		this.masu=masu;
+		this.masume = masu*masu;
 		this.mine = masume / 6;
 	}
 
-	public ArrayList<Integer> Minesweeper() {
+	public ArrayList<Integer> generateMinesweeper() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 
 		//1～masumeの整数値を持つリストを用意
-		for (int i = 1; i <= this.masume; i++) {
+		for (int i = 0; i < this.masume; i++) {
 			list.add(i);
 		}
 		System.out.println(list);
@@ -26,7 +28,7 @@ public class Minesweeper {
 
 		ArrayList<Integer> list2 = new ArrayList<Integer>();
 
-		for (int i = 1; i <= this.mine; i++) {
+		for (int i = 0; i < this.mine; i++) {
 			list2.add(list.get(i));
 		}
 		System.out.println(list2);
@@ -35,16 +37,17 @@ public class Minesweeper {
 	}
 
 	private ArrayList<Integer> MineCount(ArrayList<Integer> list) {
-		boolean[][] mineField = new boolean[this.masume][this.masume];
+		boolean[][] mineField = new boolean[this.masu][this.masu];
 		ArrayList<Integer> countList = new ArrayList<Integer>();
-		for (int i = 1; i <= this.mine; i++) {
-			int y = (int) list.get(i) / 5;
-			int x = list.get(i) - y * 5;
+		for (int i = 0; i < this.mine; i++) {
+			int index = list.get(i)-1;
+			int y = (int) index / this.masu;
+			int x = index - y * this.masu;
 			mineField[y][x] = true;
 		}
 
-		for (int y = 0; y <= 5; y++) {
-			for (int x = 0; x <= 5; x++) {
+		for (int y = 0; y < this.masu; y++) {
+			for (int x = 0; x < this.masu; x++) {
 				int count = 0;
 				for (int dy = -1; dy <= 1; dy++) {
 					for (int dx = -1; dx <= 1; dx++) {
@@ -55,7 +58,7 @@ public class Minesweeper {
 						int nx = x + dx;
 
 						// 範囲チェック
-						if (ny >= 0 && ny < 5 && nx >= 0 && nx < 5) {
+						if (ny >= 0 && ny < this.masu && nx >= 0 && nx < this.masu) {
 							if (mineField[ny][nx]) {
 								count++;
 							}
